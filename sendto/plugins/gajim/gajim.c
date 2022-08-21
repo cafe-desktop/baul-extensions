39ad4c5357de4eade4f355f191795f2528420f60
Pablo Barciela <scow@riseup.net>
2022-08-21 18:30:42 +0200

    rename CdkPixbuf -> GdkPixbuf

--- sendto/plugins/gajim/gajim.c
+++ sendto/plugins/gajim/gajim.c
@@ -272,7 +272,7 @@ init (NstPlugin *plugin)
 
 
 static void
-_set_pixbuf_from_status (const gchar *show, CdkPixbuf **pixbuf)
+_set_pixbuf_from_status (const gchar *show, GdkPixbuf **pixbuf)
 {
 	GString *pixbuf_path;
 	GError *error;
@@ -305,7 +305,7 @@ _add_contact_to_model(gpointer key, gpointer value, gpointer user_data)
 {
 	CtkTreeIter *iter;
 	CtkListStore *store;
-	CdkPixbuf *pixbuf;
+	GdkPixbuf *pixbuf;
 	GValue *val;
 	GHashTable *contact_props;
 	const gchar *show;
